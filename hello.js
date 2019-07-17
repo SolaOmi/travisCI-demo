0ae21d0ccd46826d5a695eeb4980df2d7e0aac0d
Sola <solaocodes@gmai.com>
2019-07-17 19:39:47 -0400

    Initial commit.

---
+++ hello.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var PORT = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Travis and Heroku!\n');
+}).listen(PORT, '0.0.0.0');
+
+console.log('Server running at http://127.0.0.1:1337/');
